7ffabcf73f61ff4c14bc85338dbb4ea9a131fd5e
muhilalr <muhammadhilalra@gmail.com>
2026-08-24 12:22:46 +0700

    set up filament v.5

---
+++ resources/views/layouts/app-layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en" class="scroll-smooth">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dinkominfotik | Kabupaten Bangka</title>
+
+    <!-- Tailwind / Vite -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  </head>
+  <body class="bg-[#F7F7F8]">
+
+    {{ $slot }}
+  </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<html lang="en" class="scroll-smooth">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Dinkominfotik | Kabupaten Bangka</title>
-
-    <!-- Tailwind / Vite -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-  </head>
-  <body class="bg-[#F7F7F8]">
+<x-app-layout>
     <!-- HEADER START -->
     <header class="fixed top-0 left-0 w-full z-9999 transition-colors duration-300"
       x-data="{
@@ -950,5 +940,4 @@
     </footer>
 
 
-  </body>
-</html>
+</x-app-layout>
